43d9d54b866af9ccd74f239ed51b4d58ff34dafc
Subin T P <subin.tp@truecaller.com>
2020-02-21 18:52:39 +0530

    Remove unwanted files

--- test/dummy/config/routes.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.routes.draw do
-  mount Hooks::Engine => "/hooks"
-end
--- test/models/hooks/inbox_app_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-module Hooks
-  class InboxAppTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
-  end
-end
